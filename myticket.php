397b11d60fd172086daeb92437ed40fccb63477e
Chauhan Alfiya <calfiya1@gmail.com>
2026-02-27 15:02:02 +0530

    profile updates

--- myticket.php
+++ myticket.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user_id'])) {
 
 $role  = $_SESSION['role'] ?? '';
 $email = $_SESSION['email'] ?? '';
-
+  
 switch($role) {
     case 'ADMIN':
         header("Location: admin_dashboard.php");
--- profile.php
+++ profile.php
@@ -93,3 +93,4 @@ include 'includes/header.php';
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <?php include 'includes/footer.php'; ?>
+ 
