a4f720488b63e6c7f21b0710faad439708f6b78b
Chauhan-Alfiya <calfiya1@gmail.com>
2026-05-05 20:24:00 +0530

    some file update

--- includes/send_mail.php
+++ includes/send_mail.php
@@ -1,37 +1,51 @@
-
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
-require_once __DIR__ . '/../phpMailer/PHPMailer.php';
-require_once __DIR__ . '/../phpMailer/SMTP.php';
-require_once __DIR__ . '/../phpMailer/Exception.php';
+require_once __DIR__ . '/../phpMailer/src/PHPMailer.php';
+require_once __DIR__ . '/../phpMailer/src/SMTP.php';
+require_once __DIR__ . '/../phpMailer/src/Exception.php';
 
-
-
-function sendRegisterSuccessMail($toEmail, $toName) {
+function sendRegisterSuccessMail($toEmail, $toName)
+{
     $mail = new PHPMailer(true);
-    try {
-        $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'calfiya1@gmail.com';   
-        $mail->Password   = 'mcrabydqfpjxmnkz';      
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-        $mail->Port       = 465;
 
-        $mail->setFrom('calfiya1@gmail.com', 'Helpdesk Website');
+    try {
+        // SMTP CONFIG
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+
+
+        $mail->Username = 'college.helpdesk.system@gmail.com';
+        $mail->Password = 'YOUR_APP_PASSWORD'; // 16-digit app password
+
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
+
+        
+        $mail->setFrom('YOUR_GMAIL@gmail.com', 'Helpdesk Website');
         $mail->addAddress($toEmail, $toName);
 
+        
         $mail->isHTML(true);
         $mail->Subject = 'Registration Successful';
-        $mail->Body    = "Hello $toName,<br><br>Your registration on this website was successful.<br><br>You can now log in.";
-        $mail->AltBody = "Hello $toName,\n\nYour registration on this website was successful.\n\nYou can now log in.";
 
-        $mail->send();
-        //echo 'Message sent';
+        $mail->Body = "
+            <h3>Hello $toName,</h3>
+            <p>Your registration on the Helpdesk system was successful.</p>
+            <p>You can now log in and use the system.</p>
+            <br>
+            <p>Regards,<br>Helpdesk Team</p>
+        ";
+
+        $mail->AltBody = "Hello $toName, Your registration was successful. You can now log in.";
+
+        return $mail->send();
+
     } catch (Exception $e) {
-        error_log("Email could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        error_log("Email Error: {$mail->ErrorInfo}");
+        return false;
     }
 }
